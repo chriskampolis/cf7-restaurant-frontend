72798548a2b09d2a571b6d6ead5adaa4b896cfe1
Chris Kampolis <chris.kampolis@outlook.com>
2025-08-28 11:42:02 +0300

    feat: removed refresh token

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -4,12 +4,9 @@ const Auth = {
   login: async (username: string, password: string) => {
     return api.post("/api/token/", { username, password });
   },
-  refresh: async (refresh: string) => {
-    return api.post("/api/token/refresh/", { refresh });
-  },
+
   logout: () => {
     localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
   },
 };
 
